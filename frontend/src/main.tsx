a42e9fa14706cccad3dfdebfb98d272f01e98c66
nossei9 <nana.ossei@tokunize.com>
2024-09-18 18:16:47 +0100

    Fixed Toaster Issue

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { UserProvider } from './context/userProvider';
-
 import './index.css';
 import 'leaflet/dist/leaflet.css';
 import Layout from './layout';
-import { Toaster } from "./components/ui/toaster";
-import { ToastProvider } from './components/ui/toast';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ToastProvider>
-      <Toaster />
     <Auth0Provider
       domain="dev-2l2jjwfm5ekzae3u.us.auth0.com"
       clientId="RkDK38n0VPNZEmuv0ZgQx9P93rLPAOTK"
@@ -26,6 +21,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Layout />
       </UserProvider>
     </Auth0Provider>
-    </ToastProvider>
   </React.StrictMode>,
 );
--- frontend/src/views/marketplace.tsx
+++ frontend/src/views/marketplace.tsx
@@ -1,20 +1,14 @@
 import { Herobanner } from "@/components/heroBanner";
 import { PropertyList } from "@/components/propertyList";
-import { Toaster } from "@/components/ui/toaster";
-import { ToastProvider } from "@/components/ui/toast";
 
-import { ToastDemo } from "@/components/toast";
+import { ToastDemo } from "@/components/toastDemo";
 export const Marketplace = () =>{
     return(
         <section className="px-[50px]">
-            <ToastProvider>
-            <Toaster/>
-
             <Herobanner/>
             <ToastDemo/>
             <PropertyList/>
 
-            </ToastProvider>
         </section>
     )
 }
